89c55ef20f804e339a4a3cabacb48c791f2e9d4a
KaiYC <35788426+KaiYC@users.noreply.github.com>
2024-01-10 16:41:49 +0800

    Update 00.main.js
    Update draft download

--- 00.main.js
+++ 00.main.js
@@ -1,5 +1,5 @@
 const program = {
-    'tableHead' : ['時間','議程','主持人 / 報告人'],
+    'tableHead' : ['初稿','時間','議程','主持人 / 報告人'],
     'introSession' : [
         {
             'time' : '08:30-09:00',
@@ -23,37 +23,43 @@ const program = {
             'time' : '09:30-09:40',
             'content' : '第一章：營造強健韌性幸福社會',
             'speaker' : ['主持人：國衛院群健所邱弘毅所長','報告人：國衛院群健所邱弘毅所長'],
-            'class' : 'ch1'
+            'class' : 'ch1',
+            'draft' : 'https://drive.google.com/file/d/1XmKf_kvFUZ2KGH4_8ovSXt0RCOmvebmK/view?usp=sharing'
         },
         {
             'time' : '09:40-10:05',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','何啟功教授、李玉春教授、潘文涵教授、國健署長官、中醫藥所長官(暫定)'],
-            'class' : 'ch1D'  
+            'class' : 'ch1D',
+            'draft' : ''
         },
         {
             'time' : '10:05-10:15',
             'content' : '第二章：優化友善健康與安全的生活環境',
             'speaker' : ['主持人：國衛院環醫所陳保中所長','報告人：國衛院環醫所吳威德助研究員'],
-            'class' : 'ch2'  
+            'class' : 'ch2',
+            'draft' : 'https://drive.google.com/file/d/1Y9v2WAl0i0kdlA-czO-DaYo6Stz1dDDj/view?usp=sharing'
         },
         {
             'time' : '10:15-10:40',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','李俊璋副校長、張偉嶠教授、陳明汝教授、楊振昌教授、楊榮森教授、蘇振隆教授、食藥署長官(暫定)'],
-            'class' : 'ch2D'  
+            'class' : 'ch2D',
+            'draft' : ''
         },
         {
             'time' : '10:40-10:50',
             'content' : '第三章：國人重大疾病防治',
             'speaker' : ['主持人：國衛院洪文俊主任秘書','報告人：國衛院癌研所劉柯俊所長'],
-            'class' : 'ch3'  
+            'class' : 'ch3',
+            'draft' : 'https://drive.google.com/file/d/1z4cb5G8K-uy1DXDv5mHiOdK4vMT1sWi4/view?usp=sharing'
         },
         {
             'time' : '10:50-11:15',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','林先和教授、張上淳副校長、陳宜君主任、陳垚生副院長、楊志新院長、鍾飲文院長、疾管署長官、國健署長官(暫定)'],
-            'class' : 'ch3D'
+            'class' : 'ch3D',
+            'draft' : ''
         }
     ],
     'Session2' : [
@@ -62,43 +68,50 @@ const program = {
             'time' : '11:15-11:25',
             'content' : '第四章：善用科技全面提升國民心理健康韌性',
             'speaker' : ['主持人：國衛院陳為堅副院長','報告人：國衛院神醫中心林彥鋒醫師'],
-            'class' : 'ch4'  
+            'class' : 'ch4',
+            'draft' : 'https://drive.google.com/file/d/1vbjnWI45tfcIaSqwyJ8X5WMfYCVX41R6/view?usp=sharing'  
         },
         {
             'time' : '11:25-11:50',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','田秀蘭院長、高淑芬副院長、陳柏熹教授、陳喬琪榮譽主治醫師、心健司長官(暫定)'],
-            'class' : 'ch4D'
+            'class' : 'ch4D',
+            'draft' : ''
         },
         {
             'time' : '11:50-12:50',
             'content' : '午餐',
             'speaker' : '',
-            'class' : 'sp'
+            'class' : 'sp',
+            'draft' : ''
         },
         {
             'time' : '12:50-13:05',
             'content' : '第五章：深耕優化高品質醫療服務',
             'speaker' : ['主持人：國衛院群健所邱弘毅所長','報告人：國衛院群健所陳麗光研究員'],
-            'class' : 'ch5'  
+            'class' : 'ch5',
+            'draft' : 'https://drive.google.com/file/d/1ErrE0hTjekVmoV44I5LvEVD6n-Mc75b_/view?usp=sharing'  
         },
         {
             'time' : '13:05-13:30',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','吳肖琪特聘教授、吳明賢院長、李玉春教授、楊志新院長、蔡建松局長、醫事司長官(暫定)'],
-            'class' : 'ch5D'  
+            'class' : 'ch5D',
+            'draft' : ''
         },
         {
             'time' : '13:30-13:45',
             'content' : ['第六章：精進優質照顧服務體系','第七章：完善社會福利體系'],
             'speaker' : ['主持人：國衛院高齡中心許志成執行長','報告人：國衛院高齡中心吳其炘副研究員'],
-            'class' : 'ch67'  
+            'class' : 'ch67',
+            'draft' : 'https://drive.google.com/file/d/1lMjYW0hyVNjNIVYFn4VBwdDybmkGbg9Q/view?usp=sharing'
         },
         {
             'time' : '13:45-14:10',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','王國羽教授、李淑貞主任、陳雅美教授、楊培珊教授、照護司長官、保護司長官(暫定)'],
-            'class' : 'ch67D'  
+            'class' : 'ch67D',
+            'draft' : ''
         }
     ],
     'Session3' : [
@@ -107,31 +120,36 @@ const program = {
             'time' : '14:10-14:20',
             'content' : '第八章：強化衛生福利科技基礎建設',
             'speaker' : ['主持人：國衛院群健所邱弘毅所長','報告人：國衛院生醫資源中心張新儀主任'],
-            'class' : 'ch8'  
+            'class' : 'ch8',
+            'draft' : 'https://drive.google.com/file/d/1F1-Yn3dzlIuHUZNgeH1PXQmixX6svoAh/view?usp=sharing' 
         },
         {
             'time' : '14:20-14:45',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','李佩淵名譽院長、徐建業教授、康勝閔協理、黃建寧校長、賴飛羆教授、資訊處長官、醫藥品查驗中心長官(暫定)'],
-            'class' : 'ch8D'  
+            'class' : 'ch8D',
+            'draft' : ''
         },
         {
             'time' : '14:45-15:00',
             'content' : '休息',
             'speaker' : '',
-            'class' : 'sp'
+            'class' : 'sp',
+            'draft' : ''
         },
         {
             'time' : '15:00-15:15',
             'content' : '第九章：精進暨智能化生技醫藥及健康照護產業',
             'speaker' : ['主持人：國衛院醫奈所吳嘉文所長','報告人：國衛院醫奈所吳嘉文所長'],
-            'class' : 'ch9'  
+            'class' : 'ch9',
+            'draft' : 'https://drive.google.com/file/d/1FDRfFHMSZYax3dpIzivbMG2Rna1Cm12s/view?usp=sharing'
         },
         {
             'time' : '15:15-15:40',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','吳漢章總經理、李友錚總經理、李芳蘭科長、李鍾熙理事長、廖俊仁創辦人、蕭耀貴總經理、食藥署長官、國合組長官(暫定)'],
-            'class' : 'ch9D'  
+            'class' : 'ch9D',
+            'draft' : ''
         }
     ],
     'Session4' : [
@@ -140,43 +158,50 @@ const program = {
             'time' : '15:40-15:50',
             'content' : ['整合性議題','第一章：打造高齡福祉共融生態，普惠科技推廣及應用'],
             'speaker' : ['主持人：國衛院高齡中心許志成執行長','報告人：國衛院高齡中心許志成執行長'],
-            'class' : 'intCh1'  
+            'class' : 'intCh1',
+            'draft' : 'https://drive.google.com/file/d/1S_Fnp1keXXIevQA52ezcB4Y6TZ-Q_sfJ/view?usp=sharing'  
         },
         {
             'time' : '15:50-16:15',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','李玉嬋特聘教授、李淑貞主任、林佩欣理事長、胡紀平副所長、劉燦宏副院長、長照司長官(暫定)'],
-            'class' : 'intCh1D'  
+            'class' : 'intCh1D',
+            'draft' : ''
         },
         {
             'time' : '16:15-16:25',
             'content' : ['整合性議題','第二章：因應少子化趨勢發展相關科技及照護體系'],
             'speaker' : ['主持人：國衛院群健所邱弘毅所長','報告人：國衛院群健所蔡慧如研究員'],
-            'class' : 'intCh2'  
+            'class' : 'intCh2',
+            'draft' : 'https://drive.google.com/file/d/1W2sz31hHuGqCDwrSL8IM8TVQgLaJRMCG/view?usp=sharing' 
         },
         {
             'time' : '16:25-16:50',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','王志堯院長、吳美環特聘教授、姚宗杰主治醫師、陳慧如主任醫師、黃立民院長、黃璟隆院長、醫事司長官(暫定)'],
-            'class' : 'intCh2D'  
+            'class' : 'intCh2D',
+            'draft' : ''
         },
         {
             'time' : '16:50-17:00',
             'content' : ['整合性議題','第三章：衛生福利科技相關法規'],
             'speaker' : ['主持人：國衛院群健所邱弘毅所長','報告人：國衛院群健所陳豐奇副所長'],
-            'class' : 'intCh3'  
+            'class' : 'intCh3',
+            'draft' : 'https://drive.google.com/file/d/15Ew109_IEOZcAzI3KEY4E1dir6CsI2cY/view?usp=sharing'
         },
         {
             'time' : '17:00-17:25',
             'content' : '討論 (Panel Discussion)',
             'speaker' : ['邀請專家與談','吳肖琪教授、吳明賢院長、李崇僖教授、李淑貞主任、陳鋕雄院長、健保署長官、社保司長官(暫定)'],
-            'class' : 'intCh3D'  
+            'class' : 'intCh3D',
+            'draft' : ''
         },
         {
             'time' : '17:25',
             'content' : '散會',
             'speaker' : '',
-            'class' : 'sp'  
+            'class' : 'sp',
+            'draft' : ''
         }
     ],
     'note' : '註：與談人以筆劃排序'
@@ -189,9 +214,10 @@ function programGeneration(){
         <table class="align-middle text-wrap" id="AMTable">
             <thead class="text-center">
                 <tr>
-                    <th scope="col" class="timeTopic col-1">${program.tableHead[0]}</th>
-                    <th scope="col" class="headTopic col-6">${program.tableHead[1]}</th>
-                    <th scope="col" class="col-5" >${program.tableHead[2]}</th>
+                    <th scope="col" class="driftIcon col-1">${program.tableHead[0]}</th>
+                    <th scope="col" class="timeTopic col-1">${program.tableHead[1]}</th>
+                    <th scope="col" class="headTopic col-5">${program.tableHead[2]}</th>
+                    <th scope="col" class="col-5" >${program.tableHead[3]}</th>
                 </tr>    
             </thead>
             <tbody>
@@ -200,11 +226,13 @@ function programGeneration(){
     let PMSession = ''
     PMSession += `
         <table class="align-middle text-wrap mb-3" id="PMTable">
+            <caption>${program['note']}</caption>
             <thead id="PMHead" class="text-center">
                 <tr>
-                <th scope="col" class="timeTopic col-1">${program.tableHead[0]}</th>
-                <th scope="col" class="headTopic col-6">${program.tableHead[1]}</th>
-                <th scope="col" class="col-5" >${program.tableHead[2]}</th>
+                    <th scope="col" class="driftIcon col-1">${program.tableHead[0]}</th>
+                    <th scope="col" class="timeTopic col-1">${program.tableHead[1]}</th>
+                    <th scope="col" class="headTopic col-5">${program.tableHead[2]}</th>
+                    <th scope="col" class="col-5" >${program.tableHead[3]}</th>
                 </tr>    
             </thead>
             <tbody id="PMBody">
@@ -214,6 +242,7 @@ function programGeneration(){
     for(let i=0; i<program.introSession.length; i++){
         AMSession += `
             <tr class="tableContent">
+                <td></td>
                 <td class="time">${program.introSession[i].time}</td>
         `
         //content
@@ -253,14 +282,25 @@ function programGeneration(){
             let targetSession = program[Object.keys(program)[i]]
             AMSession += `
                 <tr> 
-                    <td colspan="3" class="text-center session py-3 px-3">${targetSession[0]}</td>
+                    <td colspan="4" class="text-center session py-3 px-3">${targetSession[0]}</td>
                 </tr>
             `
             for(let j=1; j<targetSession.length; j++){
-                AMSession += `
-                <tr class="tableContent ${targetSession[j].class}" id="">
-                    <td class="time">${targetSession[j].time}</td>
-                `
+                
+                //draftDL
+                if(targetSession[j].draft != ''){
+                    AMSession +=`
+                        <tr class="tableContent ${targetSession[j].class} draftDL" id="${targetSession[j].class}" data-href="${targetSession[j].draft}">
+                            <td id="${targetSession[j].class}DL"><a href="${targetSession[j].draft}" target="_blank"><i class="bi bi-box-arrow-down dwIcon"></i></a></td>
+                    `
+                } else {
+                    AMSession +=`
+                        <tr class="tableContent ${targetSession[j].class}" id="${targetSession[j].class}">
+                            <td id="${targetSession[j].class}"></td>
+                    `
+                }
+                //time
+                AMSession +=`<td class="time">${targetSession[j].time}</td>`
                 //content
                 if(Array.isArray(targetSession[j].content)){
                     let breakContent = `${targetSession[j].content[0]}`
@@ -296,14 +336,26 @@ function programGeneration(){
             let targetSession = program[Object.keys(program)[i]]
             PMSession += `
                 <tr> 
-                    <td colspan="3" class="text-center session py-3 px-3">${targetSession[0]}</td>
+                    <td colspan="4" class="text-center session py-3 px-3">${targetSession[0]}</td>
                 </tr>
             `
             for(let j=1; j<targetSession.length; j++){
-                PMSession += `
-                <tr class="tableContent ${targetSession[j].class}">
-                    <td class="time">${targetSession[j].time}</td>
-                `
+                
+                //draftDL
+                if(targetSession[j].draft != ''){
+                    PMSession +=`
+                        <tr class="tableContent ${targetSession[j].class} draftDL" id="${targetSession[j].class}" data-href="${targetSession[j].draft}">
+                            <td id="${targetSession[j].class}DL"><a href="${targetSession[j].draft}" target="_blank"><i class="bi bi-box-arrow-down dwIcon"></i></a></td>
+                    `
+                } else {
+                    PMSession +=`
+                        <tr class="tableContent ${targetSession[j].class}" id="${targetSession[j].class}">
+                            <td id="${targetSession[j].class}DL"></td>
+                    `
+                }
+
+                //time
+                PMSession +=`<td class="time">${targetSession[j].time}</td>`
                 //content
                 if(Array.isArray(targetSession[j].content)){
                     let breakContent = `${targetSession[j].content[0]}`
@@ -395,3 +447,12 @@ window.addEventListener('resize',function(event){
    adjectTableWide();
   }
 })
+
+let draftRow = document.getElementsByClassName('draftDL')
+// console.log(draftRow)
+
+for(let i=0; i<draftRow.length; i++){
+    draftRow[i].addEventListener('click',function(){
+        window.open(`${draftRow[i].dataset.href}`,'_blank')
+    })
+}
